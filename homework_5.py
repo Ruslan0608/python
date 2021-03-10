3ac3145291666e3db1da2fbdf09ba9b428018e6f
Ruslan060874 <lrv0608@gmail.com>
2021-03-10 17:57:55 +0300

    ,

--- homework_5.py
+++ homework_5.py
@@ -51,3 +51,13 @@ klasses = [
 from itertools import zip_longest
 
 result = ((i, j) for i, j in zip_longest(tutors, klasses) if i)
+
+# 4. Представлен список чисел. Необходимо вывести те его элементы, значения которых больше предыдущего, например:
+# src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# result = [12, 44, 4, 10, 78, 123]
+
+
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result = [src[i] for i in range(1, len(src)) if src[i] > src[i-1]]
+
+
